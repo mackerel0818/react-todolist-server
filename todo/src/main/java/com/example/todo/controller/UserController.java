48e13de4b5d3883aebce83051dafa6c6dd5f15a4
makerel0818 <minji2700@naver.com>
2023-10-13 17:48:17 +0900

    Update: user

--- todo/src/main/java/com/example/todo/controller/UserController.java
+++ todo/src/main/java/com/example/todo/controller/UserController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +27,54 @@ public class UserController {
 	private UserService userService;
 	@Autowired
 	private TokenProvider tokenProvider;
+	
+	@GetMapping("/{userId}")
+    public ResponseEntity<?> getUserInfo(@PathVariable String userId) {
+        try {
+            UserEntity user = userService.getUserInfo(userId);
+
+            if (user != null) {
+                UserDTO userInfoDTO = UserDTO.builder()
+                        .id(user.getId())
+                        .username(user.getUsername())
+                        .email(user.getEmail())
+                        .build();
+
+                return ResponseEntity.ok().body(userInfoDTO);
+            } else {
+                return ResponseEntity.notFound().build(); 
+            }
+        } catch (Exception e) {
+            ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+	
+	@PutMapping("/{userId}")
+	public ResponseEntity<?> updateUserInfo(@PathVariable String userId, @RequestBody UserEntity updatedUser) {
+	    try {
+	        // userId에 해당하는 사용자 정보 가져오는 로직 수행
+	        UserEntity user = userService.getUserInfo(userId);
+
+	        if (user != null) {
+	            // 업데이트된 사용자 정보를 기존 사용자 정보에 복사
+	            user.setUsername(updatedUser.getUsername());
+	            user.setEmail(updatedUser.getEmail());
+	            // 다른 정보 업데이트
+
+	            // 업데이트된 사용자 정보 저장
+	            userService.updateUser(user);
+
+	            return ResponseEntity.ok().body("User information updated successfully");
+	        } else {
+	            return ResponseEntity.notFound().build(); // 사용자를 찾을 수 없는 경우 404 응답
+	        }
+	    } catch (Exception e) {
+	        ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+	        return ResponseEntity.badRequest().body(responseDTO);
+	    }
+	}
+
 
 	@PostMapping("/signup")
 	public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
@@ -62,4 +114,17 @@ public class UserController {
 			return ResponseEntity.badRequest().body(responseDTO);
 		}
 	}
+	
+	@DeleteMapping("/{userId}")
+    public ResponseEntity<?> deleteUser(@PathVariable String userId) {
+        try {
+            // 유저 삭제 로직을 수행
+            userService.deleteUser(userId);
+
+            return ResponseEntity.ok().body("User deleted successfully");
+        } catch (Exception e) {
+            ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
 }
--- todo/src/main/java/com/example/todo/service/UserService.java
+++ todo/src/main/java/com/example/todo/service/UserService.java
@@ -27,4 +27,26 @@ public class UserService {
 	public UserEntity getByCredentials(final String email, final String password) {
 		return userRepository.findByEmailAndPassword(email, password);
 	}
+
+	public void deleteUser(String userId) {
+		userRepository.deleteById(userId);
+		
+	}
+
+	public UserEntity getUserInfo(String userId) {
+        if (userId == null) {
+            throw new RuntimeException("Invalid user ID");
+        }
+
+        return userRepository.findById(userId)
+                .orElse(null);
+    }
+
+    public void updateUser(UserEntity userEntity) {
+        if (userEntity == null) {
+            throw new RuntimeException("Invalid user information");
+        }
+
+        userRepository.save(userEntity);
+    }
 }
